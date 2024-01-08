ba4e1b6b12183ab913b2eab43637c14cf7c20aa8
AlirezaZainy <alirezazainy16@gmail.com>
2024-01-08 15:08:03 +0330

    Update database.py

--- DB/database.py
+++ DB/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Create Database SQLAlchemy Engine
 engine = create_engine(
-    "postgresql://postgres:Alireza16@localhost/OpenMeteo")
+    "postgresql://postgres:51MOytgRlPrqMeLnbfcUPX9AtLKfAQ37@open-meteo-db.alirezazainy16.svc:5432/OpenMeteo")
 # Create Base Class of Database
 Base = declarative_base()
 # Create Database Session
